d85cdb8a1f23ea1056f070ac002f9ad5fc1b301f
Win <chetan882777@gmail.com>
2020-02-02 20:22:24 +0530

    Menu activity set

--- app/src/main/java/com/example/github/ui/restaurant/menu/MenuActivity.java
+++ app/src/main/java/com/example/github/ui/restaurant/menu/MenuActivity.java
@@ -1,16 +1,25 @@
 package com.example.github.ui.restaurant.menu;
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Adapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.github.R;
+import com.example.github.firebase.FirebaseRequestListener;
 import com.example.github.model.Menu;
+import com.example.github.ui.auth.registrationRestaurant.RegisterRestaurantViewModel;
+import com.example.github.ui.main.ViewModelProviderFactory;
+import com.example.github.util.Constants;
+import com.example.github.util.SharedPrefUtil;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
@@ -27,10 +36,18 @@ public class MenuActivity extends DaggerAppCompatActivity {
     public static final String MENU_FRESH_ADD = "MENU_FRESH_ADD";
     public static final String MENU_EXIST_EDIT = "MENU_EXIST_EDIT";
 
+    public static final String MENU_TYPE_VEG = "Veg";
+    public static final String MENU_TYPE_NON_VEG = "Non Veg";
+
     private RecyclerView menuRecyclerView;
     private Button addMenuButton;
+    private Button saveMenuButton;
     private MenuAdapter menuAdapter;
     private List<Menu> menuList = new ArrayList<>();
+
+    @Inject
+    ViewModelProviderFactory providerFactory;
+    MenuViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +57,85 @@ public class MenuActivity extends DaggerAppCompatActivity {
         Intent intent = getIntent();
         String msg = intent.getStringExtra(INTENT_MENU_MSG);
 
+        viewModel = ViewModelProviders.of(this, providerFactory).get(MenuViewModel.class);
+
         addMenuButton = findViewById(R.id.button_addMenu);
+        saveMenuButton = findViewById(R.id.button_addMenu_save);
         setAdapter();
+        setSaveMenu();
 
 
         if(msg.equals(MENU_FRESH_ADD)){
-            Snackbar.make(addMenuButton, "Click add menu button to add some menu",
-                    Snackbar.LENGTH_LONG).show();
-                addMenuButton.setOnClickListener(v -> {
-
-                });
+            showMessage("Click add menu button to add some menu");
+            setAddMenuClick();
         }else{
 
         }
+    }
+
+    private void setSaveMenu() {
+        saveMenuButton.setOnClickListener(v -> {
+
+            FirebaseRequestListener<String> listener = data -> {
+                showMessage(data);
+                if(data.equals(Constants.FIREBASE_SUCCESS)){
+
+                }
+            };
+
+            SharedPrefUtil sharedPrefUtil = new SharedPrefUtil(MenuActivity.this);
+            String credentials = sharedPrefUtil.getCredentials();
+
+            viewModel.saveMenus(credentials, menuList, listener);
+        });
+    }
+
+    private void setAddMenuClick() {
+        addMenuButton.setOnClickListener(v -> {
+            final Dialog dialog = new Dialog(MenuActivity.this);
+            dialog.setContentView(R.layout.add_menu_dialog);
+            dialog.setTitle("Add Menu");
+            RadioGroup radioGroup = dialog.findViewById(R.id.radioGroup_menuType);
+
+            Button add = dialog.findViewById(R.id.button_addMenuDialog_add);
+            Button cancel = dialog.findViewById(R.id.button_addMenuDialog_cancel);
+
+            EditText menuName = dialog.findViewById(R.id.editText_addMenuDialog_menuName);
+            EditText menuPrice = dialog.findViewById(R.id.editMenu_addMenuDialog_menuPrice);
+
+            add.setOnClickListener(v2 -> {
+                setValueToList(radioGroup, menuName, menuPrice);
+                dialog.dismiss();
+            });
+
+            cancel.setOnClickListener(v2 -> {
+                dialog.dismiss();
+            });
+            dialog.show();
+        });
+    }
+
+    private void setValueToList(RadioGroup radioGroup, EditText menuName, EditText menuPrice) {
+        String menuType = MENU_TYPE_VEG;
+        int radioSelected = radioGroup.getCheckedRadioButtonId();
+        if(radioSelected == R.id.radioVeg){
+            menuType = MENU_TYPE_VEG;
+        }else if(radioSelected == R.id.radioNonVeg){
+            menuType = MENU_TYPE_NON_VEG;
+        }
+        String name = menuName.getText().toString();
+        String price = menuPrice.getText().toString();
+
+        if(name.isEmpty() || price.isEmpty()){
+            showMessage("Any of the field cannot be empty");
+            return;
+        }
+
+        Menu menu = new Menu(name, price, menuType);
+
+        menuList.add(menu);
+        menuAdapter.setMenus(menuList);
+        menuAdapter.notifyDataSetChanged();
     }
 
     private void setAdapter() {
@@ -67,5 +150,12 @@ public class MenuActivity extends DaggerAppCompatActivity {
 
         menuRecyclerView.setLayoutManager(layoutManager);
         menuRecyclerView.setAdapter(menuAdapter);
+    }
+
+    private void showMessage(String s) {
+        Snackbar.make(addMenuButton,
+                s,
+                Snackbar.LENGTH_SHORT)
+                .show();
     }
 }
--- app/src/main/java/com/example/github/ui/restaurant/menu/MenuViewModel.java
+++ app/src/main/java/com/example/github/ui/restaurant/menu/MenuViewModel.java
@@ -1,11 +1,36 @@
 package com.example.github.ui.restaurant.menu;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+
+import com.example.github.firebase.FirebaseRequestListener;
+import com.example.github.model.Menu;
+import com.example.github.util.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 public class MenuViewModel extends ViewModel {
 
     @Inject
-    public MenuViewModel(){}
+    public MenuViewModel() {
+    }
+
+    public void saveMenus(String restaurant, List<Menu> menus, FirebaseRequestListener<String> listener) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference reference = database.getReference("restaurant/" + restaurant + "/menus");
+
+        reference.setValue(menus).addOnCompleteListener(task -> {
+            listener.OnFirebaseRequest(Constants.FIREBASE_SUCCESS);
+        }).addOnFailureListener(e -> {
+            listener.OnFirebaseRequest(Constants.FIREBASE_FAILED);
+        });
+    }
 }
